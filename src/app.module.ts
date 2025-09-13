6c0f21df555e937df8ab3fd930382d05a397f25d
Het Suthar <het.suthar@wishtreetech.com>
2025-09-13 13:33:39 +0530

    Employees added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
